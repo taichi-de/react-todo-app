26f7c1702d9e5c8979d672d83e88141b35cb03f5
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-02-22 15:58:22 +0100

    add webpack

--- src/index.js
+++ src/index.js
@@ -2,22 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class CountApp extends Component {
-  constructor() {
-    ...
-  }
+// // 関数ベース
+// const TodoApp = () => {
+//   const [value, setValue] = useState("");
+//   const [todoList, setTodoList] = useState([]);
 
-  plus() {
-    this.setState({count: this.state.count + 1})
-  }
+//   const handleChange = e => {
+//     const newValue = e.target.value;
+//     setValue(newValue);
+//   };
 
-  render() {
-    // ...
-  }
-}
+//   const add = () => {
+//     const newTodo = [...todoList, value];
+//     setTodoList(newTodo);
+//     setValue("");
+//   };
 
-<input
-　　type="text"
-　　value={value}
-　　onChange={e => handleChange(e)}
-/>
+//   return (
+//     <div>
+//       <h1>TODO App</h1>
+//       <div>
+//         <input type="text" value={value} onChange={e => handleChange(e)} />
+//         <ul>
+//           {todoList.map((todo, i) => (
+//             <li key={i}>{todo}</li>
+//           ))}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+//  };
+
+ ReactDOM.render(
+   <HelloWorld />,
+   document.getElementById('root')
+ );
