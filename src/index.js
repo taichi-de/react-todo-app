0121813c0d9f9ea44c868fc0fb89af3fefac7b58
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-02-22 15:16:06 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+class CountApp extends Component {
+  constructor() {
+    ...
+  }
+
+  plus() {
+    this.setState({count: this.state.count + 1})
+  }
+
+  render() {
+    // ...
+  }
+}
+
+<input
+　　type="text"
+　　value={value}
+　　onChange={e => handleChange(e)}
+/>
